27c4af865207865a031c06efb69077667952ad01
ShreyaSrivastava12 <91174445+ShreyaSrivastava12@users.noreply.github.com>
2022-09-06 21:39:52 +0530

    Create Pattern9.cc

---
+++ Patterns/Pattern9.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   int k = 0;
+   for (int i = 1; i <=5; i++, k = 0) 
+   {
+      for (int j = 1; j <= 5 - i; j++) 
+      {
+        cout<<"  ";
+      }
+      while (k != 2 * i - 1) 
+      {
+        cout<<"* ";
+        k++;
+      }
+      cout<<endl;
+   }
+   for (int i = 5; i >=1; i--, k = 0) 
+   {
+      for (int j = 1; j <= 5 - i; j++) 
+      {
+        cout<<"  ";
+      }
+      while (k != 2 * i - 1) 
+      {
+        cout<<"* ";
+        k++;
+      }
+      cout<<endl;
+   }
+}
+/*   *
+    ***
+   *****
+  *******
+ *********
+ *********
+  *******
+   *****
+    ***
+     *    */
