b802f272b55ccfb57276e3e073d4ad7bda316fb7
ShreyaSrivastava12 <91174445+ShreyaSrivastava12@users.noreply.github.com>
2022-09-07 19:47:46 +0530

    Update Pattern1.cc

--- Patterns/Pattern1.cc
+++ Patterns/Pattern1.cc
@@ -1,15 +1,23 @@
 #include <iostream>
 using namespace std;
-int main()
+void pattern(int n)
 {
-    for(int i=1;i<=5;i++)
+    int i,j;
+    for(i=0;i<n;i++)
     {
-        for(int j=1;j<=5;j++)
+        for(j=0;j<n;j++)
         {
-          cout<<"*";
+            cout<<"*";
         }
         cout<<endl;
     }
+}
+int main()
+{
+    int n;
+    cin>>n;
+    pattern(n);
+    return 0;
 }
 /* *****
    *****
