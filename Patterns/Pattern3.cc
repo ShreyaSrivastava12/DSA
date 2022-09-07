7785861b9d70f4988aeb2c3dc72472c8e8c0e7b9
ShreyaSrivastava12 <91174445+ShreyaSrivastava12@users.noreply.github.com>
2022-09-07 19:51:19 +0530

    Update Pattern3.cc

--- Patterns/Pattern3.cc
+++ Patterns/Pattern3.cc
@@ -1,15 +1,23 @@
 #include <iostream>
 using namespace std;
-int main()
+void pattern(int n)
 {
-    for(int i=1;i<=5;i++)
+    int i,j;
+    for(i=0;i<n;i++)
     {
-        for(int j=1;j<=i;j++)
+        for(j=0;j<i+1;j++)
         {
-          cout<<j;
+            cout<<j+1;
         }
         cout<<endl;
     }
+}
+int main()
+{
+    int n;
+    cin>>n;
+    pattern(n);
+    return 0;
 }
 /* 1
    12
