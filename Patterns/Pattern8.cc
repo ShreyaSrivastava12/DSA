1a13c2c2b0593696f0fc6e369fb3fecd3269bced
ShreyaSrivastava12 <91174445+ShreyaSrivastava12@users.noreply.github.com>
2022-09-07 20:12:28 +0530

    Update Pattern8.cc

--- Patterns/Pattern8.cc
+++ Patterns/Pattern8.cc
@@ -5,6 +5,18 @@ void pattern(int n)
     int i,j;
     for(i=0;i<n;i++)
     {
+        for(int k=0;k<n-i-1;k++)
+        {
+            cout<<" ";
+        }
+        for(j=0;j<2*i+1;j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for(i=0;i<n;i++)
+    {
         for(int k=0;k<i;k++)
         {
             cout<<" ";
